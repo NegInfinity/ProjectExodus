2abce6d0ac8eb2e31c46b57d18cd8436c24a7039
Victor 'NegInfinity' Eremin <altctrlsysrqb@gmail.com>
2019-04-24 15:35:04 +0300

    possible fix for incorrect decimal separator used on floats

--- ExodusExport/Scripts/Editor/Exporter/FastJsonWriter/RawCoreTypes.cs
+++ ExodusExport/Scripts/Editor/Exporter/FastJsonWriter/RawCoreTypes.cs
@@ -4,27 +4,34 @@ using System.IO;
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace SceneExport{
 	public partial class FastJsonWriter{
+		static readonly CultureInfo writerCulture = CultureInfo.InvariantCulture;//new CultureInfo()
 		public void writeRawValue(string s){
 			writeString(s);
 		}
 		
 		public void writeRawValue(int val){
-			builder.Append(val);
+			//builder.Append(val);
+			builder.Append(val.ToString(writerCulture));
 		}
 
 		public void writeRawValue(ResId val){
-			builder.Append(val.rawId);
+			//builder.Append(val.rawId);
+			builder.Append(val.rawId.ToString(writerCulture));
 		}
 
 		public void writeRawValue(byte val){
-			builder.Append(val);
+			//builder.Append(val);
+			builder.Append(val.ToString(writerCulture));
 		}
 
 		public void writeRawValue(float val){
-			builder.Append(val);
+			//builder.Append(val);
+			//This is necessary, because on some systems CurrentCulture returns culutre thath as "," as decimal separator, which breaks json code.
+			builder.Append(val.ToString(writerCulture));
 		}
 		
 		public void writeRawValue(bool b){
